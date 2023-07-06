ec4efc317cdc4dbe9a422b765b37f172081af8bb
Sergey Koksharov <gentoosiastic@gmail.com>
2023-07-06 22:15:15 +0300

    feat: add TypeScript, ESLint, Prettier, type definitions

---
+++ src/http_server/index.ts
@@ -0,0 +1,17 @@
+import fs from 'node:fs';
+import http from 'node:http';
+import path from 'node:path';
+
+export const httpServer = http.createServer(function (req, res) {
+  const __dirname = path.resolve(path.dirname(''));
+  const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+  fs.readFile(file_path, function (err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+});
