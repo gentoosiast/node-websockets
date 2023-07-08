6cf642bbada2cbb7f08d1d9e44c2e1b29a80c2ce
Sergey Koksharov <gentoosiastic@gmail.com>
2023-07-08 21:25:58 +0300

    feat: create implementation of high score table

---
+++ src/store/score-table.ts
@@ -0,0 +1,22 @@
+import { Winner } from '../types/player.js';
+
+const SCORE_TABLE_ENTRIES = 10;
+
+class ScoreTable {
+  private table: Map<string, number> = new Map(); // player name, number of wins
+
+  addWinner(playerName: string): void {
+    const playerWins = this.table.get(playerName) ?? 0;
+
+    this.table.set(playerName, playerWins + 1);
+  }
+
+  getTopTenPlayers(): Winner[] {
+    return Array.from(this.table)
+      .map((entry) => ({ name: entry[0], wins: entry[1] }))
+      .sort((winnerA, winnerB) => winnerB.wins - winnerA.wins)
+      .slice(0, SCORE_TABLE_ENTRIES);
+  }
+}
+
+export const highScores = new ScoreTable(); // singleton
