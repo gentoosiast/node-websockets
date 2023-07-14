29f705096e1b3d1fcaf2c27df513c3a7bcd41a2d
Sergey Koksharov <gentoosiastic@gmail.com>
2023-07-14 22:32:17 +0300

    refactor: simplify sendCreateGameResponse()

--- src/handlers.ts
+++ src/handlers.ts
@@ -148,12 +148,12 @@ const broadcastUpdateRooms = (playerStore: PlayerStore, roomStore: RoomStore): v
   playerStore.broadcast(stringifyMessage(createUpdateRoomsResponse(roomStore)));
 };
 
-export const sendCreateGameResponse = (player: Player, gameId: number, otherPlayedId: number): void => {
+export const sendCreateGameResponse = (player: Player, gameId: number): void => {
   const message: CreateGameResponse = {
     type: MessageType.CreateGame,
     data: {
       idGame: gameId,
-      idPlayer: otherPlayedId,
+      idPlayer: player.getId(),
     },
     id: 0,
   };
@@ -210,8 +210,8 @@ export const handleAddPlayerToRoom = (
   roomStore.delete(roomId);
 
   broadcastUpdateRooms(playerStore, roomStore);
-  sendCreateGameResponse(player, gameId, player.getId());
-  sendCreateGameResponse(otherPlayer, gameId, otherPlayer.getId());
+  sendCreateGameResponse(player, gameId);
+  sendCreateGameResponse(otherPlayer, gameId);
 };
 
 export const createTurnResponse = (playerId: number): TurnResponse => {
